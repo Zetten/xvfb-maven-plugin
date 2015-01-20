0a5ce09c530d5a905149c7f20c46645e419f70dd
Peter van Zetten <peter.van.zetten@gmail.com>
2015-01-20 14:11:16 +0100

    Fix integration test script
    Undo a misplaced attempt at optimisation due to inexperience with
    Groovy.

--- src/it/simple-it/verify.groovy
+++ src/it/simple-it/verify.groovy
@@ -20,18 +20,16 @@
 
 def foundDisplay = false
 new File(basedir, "target/out-environment").eachLine { line ->
-	if ((line =~ /^DISPLAY=:99$/).matches()) {
-		foundDisplay = true
-		break
+	if (!foundDisplay) {
+		foundDisplay = (line =~ /^DISPLAY=:99$/).matches()
 	}
 }
 assert foundDisplay
 
 def foundFbdir = false
 new File(basedir, "target/out-fbdir").eachLine { line ->
-	if ((line =~ /^Xvfb_screen0$/).matches()) {
-		foundFbdir = true
-		break
+	if (!foundFbdir) {
+		foundFbdir = (line =~ /^Xvfb_screen0$/).matches()
 	}
 }
 assert foundFbdir
